1cb7b5c9c8c8892510ca113c0203f332f31b0d5a
mgthomas99 <m.g.thomas99@gmail.com>
2017-01-24 00:23:31 +0000

    feat: Weapon stats

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ class Weapon {
     static random() {
         while (true) {
             let weapon = ArrayUtils.randomElement(inv.weapons);
-            console.log(weapon);
             let weapon_class = inv.classes[ArrayUtils.randomElement(weapon.classes)];
 
             if (Math.random() * 100 <= weapon_class.chance) {
@@ -12,16 +11,18 @@ class Weapon {
                 return new Weapon(
                     w_prefix,
                     weapon.name,
-                    weapon_class
+                    weapon_class,
+                    weapon.type
                 );
             }
         }
     }
 
-    constructor(prefix, name, clas) {
+    constructor(prefix, name, clas, type) {
         this.prefix = prefix;
         this.name = name;
         this.clas = clas;
+        this.type = type;
     }
 
 }
@@ -31,14 +32,16 @@ var inv;
 function create() {
     let weapon = Weapon.random();
 
-    append("weapon_name", weapon.name);
-    append("weapon_prefix", weapon.prefix);
+    attrib("weapon_name", weapon.name);
+    attrib("weapon_prefix", weapon.prefix);
+    attrib("weapon_type", weapon.type);
+    attrib("weapon_color", weapon.clas.color);
 }
 
 (function() {
     let xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open("GET", "src/inv-index.json", true);
+    xobj.open("GET", "src/inv-index.json");
     xobj.onreadystatechange = function() {
         if (this.readyState === 4) {
             let json = this.responseText;
