92027d06ac4d0b2e69a34e80243e2ca99e2de9a4
mgthomas99 <m.g.thomas99@gmail.com>
2017-01-25 00:48:53 +0000

    updated utils.js

--- src/util.js
+++ src/util.js
@@ -1,10 +1,25 @@
-
+/**
+ * Utility API for array manipulation.
+ */
 class ArrayUtils {
 
-    static combine(arr1, arr2) {
-        return arr1;
+    /**
+     * Returns a new array which contains all elements from all argument
+     * arrays.
+     */
+    static combine(...arrays) {
+        let arr = [];
+        for (let a of arrays) {
+            if (typeof(a) == []) {
+                arr += a;
+            }
+        }
+        return arr;
     }
 
+    /**
+     * Returns a random element from the argument array.
+     */
     static randomElement(arr) {
         if (! arr || arr.length === 0) {
             return null;
