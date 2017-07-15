2e7b42cafab9700fe1f37e5dda640b2a4f3d1dbb
mgthomas99 <m.g.thomas99@gmail.com>
2017-07-15 22:00:39 +0100

    refactored server

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,17 @@ import { Application, Request, Response } from "express";
 import * as express from "express"
 import * as path from "path";
 
-import { create } from "./../dead-island";
-
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + "/../../public"));
+app.use(express.static(__dirname + "/../public"));
 
 app.get("/", (req: Request, res: Response) => {
 	res.sendFile(path.resolve("public/index.html"));
 });
 
 app.get("/weapon", (req: Request, res: Response) => {
-	return create();
+	res.send("Machete Mk. II");
 });
 
 app.listen(port);
